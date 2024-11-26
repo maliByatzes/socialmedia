4fb19b756be258679a02ad416556413d9afeaa03
maliByatzes <malib2027@gmail.com>
2024-11-26 16:46:38 +0200

    implement some `community_member` db logic

--- server/postgres/community_member.go
+++ server/postgres/community_member.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"context"
+	"fmt"
 
 	sm "github.com/maliByatzes/socialmedia"
 )
@@ -58,10 +59,75 @@ func (s *CommunityMemberService) DeleteCommunityMember(ctx context.Context, id u
 }
 
 func findCommunityMembers(ctx context.Context, tx *Tx, filter sm.CommunityMemberFilter) (_ []*sm.CommunityMember, n int, err error) {
+	where, args := []string{}, []interface{}{}
+	argPos := 1
 
+	if v := filter.CommunityID; v != nil {
+		where, args = append(where, fmt.Sprintf(`"community_id" = $%d`, argPos)), append(args, *v)
+		argPos++
+	}
+
+	if v := filter.UserID; v != nil {
+		where, args = append(where, fmt.Sprintf(`"user_id" = $%d`, argPos)), append(args, *v)
+		argPos++
+	}
+
+	if v := filter.IsModerator; v != nil {
+		where, args = append(where, fmt.Sprintf(`"is_moderator" = $%d`, argPos)), append(args, *v)
+	}
+
+	query := `SELECT "community_id", "user_id", "is_moderator", "created_at", "updated_at", COUNT(*) OVER()
+	FROM "community_members"` + formatWhereClause(where) + ` ORDER BY id ASC` + formatLimitOffset(filter.Limit, filter.Offset)
+
+	rows, err := tx.QueryContext(ctx, query, args...)
+	if err != nil {
+		return nil, n, err
+	}
+	defer rows.Close()
+
+	cms := make([]*sm.CommunityMember, 0)
+	for rows.Next() {
+		var cm sm.CommunityMember
+		if err := rows.Scan(
+			&cm.CommunityID,
+			&cm.UserID,
+			&cm.IsModerator,
+			(*NullTime)(&cm.CreatedAt),
+			(*NullTime)(&cm.UpdatedAt),
+			&n,
+		); err != nil {
+			return nil, n, err
+		}
+
+		cms = append(cms, &cm)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, 0, err
+	}
+
+	return cms, n, nil
 }
 
-func createCommunityMember(ctx context.Context, tx *Tx, cm *sm.CommunityMember) error {}
+func createCommunityMember(ctx context.Context, tx *Tx, cm *sm.CommunityMember) error {
+	cm.CreatedAt = tx.now
+	cm.UpdatedAt = cm.CreatedAt
+
+	query := `INSERT INTO "community_members"("community_id", "user_id", "is_moderator", "created_at", "updated_at")
+	VALUES ($1, $2, $3, $4, $5)`
+	args := []interface{}{
+		cm.CommunityID,
+		cm.UserID,
+		cm.IsModerator,
+		(*NullTime)(&cm.CreatedAt),
+		(*NullTime)(&cm.UpdatedAt),
+	}
+
+	if _, err := tx.ExecContext(ctx, query, args...); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func updateCommunityMember(ctx context.Context, tx *Tx, id uint, upd sm.CommunityMemberUpdate) (*sm.CommunityMember, error) {
 }
